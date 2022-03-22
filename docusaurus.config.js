b6c55492b17524c762915067c6132ac7a12cdfbc
Sakrecoer <public@sakrecoer>
2022-03-22 15:59:20 +0100

    re-label link in header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
             type: "doc",
             docId: "intro",
             position: "left",
-            label: "Tutorial",
+            label: "Documentation",
           },
           {
             href: "https://github.com/alephium",
