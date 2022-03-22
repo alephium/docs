3938719b06934d91f3c09376eda45a381da78646
Sakrecoer <public@sakrecoer>
2022-03-22 14:48:25 +0100

    customize CTA on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,8 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            Get started with Alephium 🫕
           </Link>
         </div>
       </div>
@@ -30,8 +27,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title} Documentation`}
+      description="The 1st operational sharded blockchain scaling and improving on PoW &amp; UTXO to deliver a highly performant, secure and energy efficient DeFi &amp; dApps platform."
     >
       <HomepageHeader />
       <main>
